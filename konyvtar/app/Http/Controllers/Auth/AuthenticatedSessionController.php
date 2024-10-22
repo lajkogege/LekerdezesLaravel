a7043f99f93028c712bab7f35327cf80c83cd855
Agnes-milia <ratipotti@gmail.com>
2024-10-22 14:45:52 +0200

    autentikáció pipa

--- konyvtar/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ konyvtar/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -13,26 +13,33 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request)
     {
-        $request->authenticate();
-
-        $request->session()->regenerate();
-
-        return response()->noContent();
+        $request->validate([
+            'email' => ['required', 'string', 'email'],
+                'password' => ['required', 'string'],
+            ]);
+    if (!Auth::attempt($request->only('email', 'password'))) {
+                return response()->json(['message' => 'Invalid login credentials'], 401);        }
+    
+    $user = Auth::user();
+    $token = $user->createToken('auth_token')->plainTextToken;
+    return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user,
+                'status' => 'Login successful',
+            ]);
+    
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request)
     {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return response()->noContent();
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logout successful']);
     }
+
 }
--- konyvtar/routes/api.php
+++ konyvtar/routes/api.php
@@ -1,8 +1,31 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+//bárki által elérhető
+Route::post('/register',[RegisteredUserController::class, 'store']);
+Route::post('/login',[AuthenticatedSessionController::class, 'store']);
+
+//autentikált útvonal
+Route::middleware(['auth:sanctum'])
+    ->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        // Kijelentkezés útvonal
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+    });
+
+//admin
+Route::middleware(['auth:sanctum',Admin::class])
+->group(function () {
+    Route::get('/admin/users', [UserController::class, 'index']);
 });
+
+
+
